31e4c276d4eef9a32df9674d43e2e9266c4a94b0
Linquize <linquize@yahoo.com.hk>
2013-12-26 10:01:25 +0800

    Add ProtoBuf.Project library

---
+++ ProtoBuf.Project/MSBuildProject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace ProtoBuf.Project
+{
+    public class MSBuildProject
+    {
+        const string msbuildNS = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        XmlDocument xd;
+
+        public void Open(string projectFile)
+        {
+            xd = new XmlDocument();
+            xd.Load(projectFile);
+        }
+
+        public void Init(string protoGenPath)
+        {
+            var xnm = new XmlNamespaceManager(xd.NameTable);
+            xnm.AddNamespace("msbuild", msbuildNS);
+
+            string importProject = @"$(ProtoGenPath)\ProtoBuf.MSBuildTask.targets";
+            var importNode = xd.SelectSingleNode("/msbuild:Project/msbuild:Import[@Project='" + importProject + "']", xnm);
+            if (importNode == null)
+            {
+                var xe = xd.CreateElement("Import", msbuildNS);
+                xe.SetAttribute("Project", importProject);
+                importNode = xe;
+                xd.DocumentElement.AppendChild(xe);
+            }
+
+            var protoGenPathNode = xd.SelectSingleNode("/msbuild:Project/msbuild:PropertyGroup[not(@Condition)]/msbuild:ProtoGenPath", xnm);
+            if (protoGenPathNode == null)
+            {
+                var xe = xd.CreateElement("PropertyGroup", msbuildNS);
+                var xe2 = xd.CreateElement("ProtoGenPath", msbuildNS);
+                xe2.InnerText = protoGenPath;
+                xe.AppendChild(xe2);
+                // ensure properties come before targets
+                xd.DocumentElement.InsertBefore(xe, importNode);
+            }
+        }
+
+        public void AddItem(string protoFile)
+        {
+            var xnm = new XmlNamespaceManager(xd.NameTable);
+            xnm.AddNamespace("msbuild", msbuildNS);
+
+            var xe2 = xd.CreateElement("ProtoBuf", msbuildNS);
+            xe2.SetAttribute("Include", protoFile);
+            var xe3 = xd.CreateElement("Generator", msbuildNS);
+            xe3.InnerText = "MSBuild:Compile";
+            xe2.AppendChild(xe3);
+
+            var protoBufNode = xd.SelectSingleNode("/msbuild:Project/msbuild:ItemGroup/msbuild:ProtoBuf", xnm);
+            if (protoBufNode == null)
+            {
+                protoBufNode = xd.CreateElement("ItemGroup", msbuildNS);
+                var itemGroupNodes = xd.SelectNodes("/msbuild:Project/msbuild:ItemGroup", xnm);
+                if (itemGroupNodes.Count > 0)
+                    xd.DocumentElement.InsertAfter(protoBufNode, itemGroupNodes[itemGroupNodes.Count - 1]);
+                else
+                    xd.DocumentElement.AppendChild(protoBufNode);
+                protoBufNode.AppendChild(xe2);
+            }
+            else
+                protoBufNode.ParentNode.AppendChild(xe2);
+        }
+
+        public void Save(string projectFile)
+        {
+            xd.Save(projectFile);
+        }
+    }
+}
---
+++ ProtoBuf.Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProtoBuf.Project")]
+[assembly: AssemblyDescription("ProtoBuf Project Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Linquize")]
+[assembly: AssemblyProduct("ProtoBuf.MSBuildTask")]
+[assembly: AssemblyCopyright("Copyright © 2013 Linquize")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e8be50a7-b1c3-4689-9705-30347d4a5814")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
