bdfa7d42dee5b42e6299bbf249a6b832fa162ba6
Linquize <linquize@yahoo.com.hk>
2014-06-16 00:12:06 +0800

    Log error message of protogen.exe so user knows what's wrong

--- ProtoBuf.MSBuildTask/ProtoGen.cs
+++ ProtoBuf.MSBuildTask/ProtoGen.cs
@@ -43,8 +43,52 @@ namespace ProtoBuf.MSBuildTask
             }
 
             Log.LogCommandLine(ProtoGenExecutable + " " + sb.ToString());
-            var process = Process.Start(new ProcessStartInfo(ProtoGenExecutable, sb.ToString()) { CreateNoWindow = true, UseShellExecute = false });
+            var process = Process.Start(new ProcessStartInfo(ProtoGenExecutable, sb.ToString()) { CreateNoWindow = true, UseShellExecute = false, RedirectStandardError = true });
+            string error = process.StandardError.ReadToEnd();
             process.WaitForExit();
+            if (process.ExitCode == 0)
+            {
+                if (error.Length > 0)
+                    Log.LogWarning(error);
+            }
+            else
+            {
+                Log.LogError("protogen failed with exit code {0}", process.ExitCode);
+                var reader = new StringReader(error);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    bool parsed = false;
+                    do
+                    {
+                        int i1 = line.IndexOf(':');
+                        if (i1 < 0)
+                            continue;
+                        int i2 = line.IndexOf(':', i1 + 1);
+                        if (i2 < 0)
+                            continue;
+                        int i3 = line.IndexOf(':', i2 + 1);
+                        if (i3 < 0)
+                            continue;
+                        string filename = line.Substring(0, i1);
+                        string slineNum = line.Substring(i1 + 1, i2 - i1 - 1);
+                        int lineNum;
+                        if (!int.TryParse(slineNum, out lineNum))
+                            continue;
+                        string scolNum = line.Substring(i2 + 1, i3 - i2 - 1);
+                        int colNum;
+                        if (!int.TryParse(scolNum, out colNum))
+                            continue;
+                        string message = i3 + 1 < line.Length ? line.Substring(i3 + 1) : "";
+                        Log.LogError("", "", "", filename, lineNum, colNum, lineNum, colNum, message);
+                        parsed = true;
+                    }
+                    while (false);
+                    if (!parsed)
+                        Log.LogError("protogen error: {0}", line);
+                }
+            }
+
             list.Add(new TaskItem(output));
             GeneratedCodeFiles = list.ToArray();
             return true;
